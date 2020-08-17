cae84aee17fc0578184615430703e2c9a138d56a
Katiuska <katiuskaalicea@gmail.com>
2020-08-16 22:44:41 -0700

    cannot validate form, so I removed the disabled attribute so I could continue on working on the other MVP requirements. Will come back to it

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,8 +3,6 @@ import styled from "styled-components"
 import * as yup from 'yup'
 import axios from 'axios'
 
-
-
 const Form = () => {
 
     //form initial state
@@ -21,7 +19,7 @@ const Form = () => {
         tomatoes: false,
         olives: false,
         roastedGarlic: false,
-        artichocke: false,
+        artichoke: false,
         cheese: false,
         pineapple: false,
         extraCheese: false,
@@ -43,7 +41,7 @@ const Form = () => {
         tomatoes: '',
         olives: '',
         roastedGarlic: '',
-        artichocke: '',
+        artichoke: '',
         cheese: '',
         pineapple: '',
         extraCheese: '',
@@ -54,7 +52,7 @@ const Form = () => {
     //form schema
     const orderSchema = yup.object().shape({
         size: yup.string().oneOf(["Personal", "Medium", "Large", "X-Large"], 'Please select a size'),
-        sauce: yup.string().required('Please choose a sauce'),
+        sauce: yup.string().oneOf(['original', 'garlic', 'BBQ', 'alfredo'])/* required('Please choose a sauce'), */,
         pepperoni: yup.string().notRequired(),
         sausage: yup.string().notRequired(),
         bacon: yup.string().notRequired(),
@@ -110,6 +108,7 @@ const Form = () => {
     //listen for input changes 
     const inputChange = (e) => {
         e.persist()
+        e.target.type === 'checkbox' ? console.log(e.target.name) : console.log(e.target.value)
         const newOrder = {
             ...orderState,
             [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
@@ -118,7 +117,7 @@ const Form = () => {
         setOrderState(newOrder)
     }
 
-    //sumbit order
+    //Submit order
     const orderSubmit = (e) => {
         e.preventDefault()
         axios.post('https://reqres.in/api/users', orderState)
@@ -138,7 +137,7 @@ const Form = () => {
                 tomatoes: false,
                 olives: false,
                 roastedGarlic: false,
-                artichocke: false,
+                artichoke: false,
                 cheese: false,
                 pineapple: false,
                 extraCheese: false,
@@ -167,34 +166,34 @@ const Form = () => {
                 Choice of Sauce: 
                 <br /> 
                 <label htmlFor="original">
-                <input type="radio" name="sauce" id="original" /> 
+                <input type="radio" name="sauce" id="original" value="original" onChange={inputChange}/> 
                 Original Red
                 </label>
                 <br/>
                 <label htmlFor="garlic">
-                <input type="radio" name="sauce" id="garlic" /> 
+                <input type="radio" name="sauce" id="garlic" value="garlic" onChange={inputChange} /> 
                 Garlic Ranch
                 </label>
                 <br/>
                 <label htmlFor="BBQ">
-                <input type="radio" name="sauce" id="BBQ" /> 
+                <input type="radio" name="sauce" id="BBQ" value="BBQ" onChange={inputChange} /> 
                 BBQ Sauce
                 </label>
                 <br/>
                 <label htmlFor="alfredo">
-                <input type="radio" name="sauce" id="alfredo"/> 
+                <input type="radio" name="sauce" id="alfredo" value="alfredo" onChange={inputChange} /> 
                 Spinach Alfredo
                 </label>
                 <br/>
                 Add Toppings:<br />
                 Add up to 10<br />
                 <label htmlFor="pepperoni">
-                    <input type="checkbox" name="pepperoni" id="pepperoni" onChange={inputChange} />
+                    <input type="checkbox" name="pepperoni" id="pepperoni" onChange={inputChange} checked={orderState.pepperoni} />
                     Pepperoni
                 </label>
                 <br /> 
                 <label htmlFor="sausage">
-                    <input type="checkbox" name="sausage" id="sausage" onChange={inputChange} />
+                    <input type="checkbox" name="sausage" id="sausage" onChange={inputChange} checked={orderState.sausage} />
                     Sausage
                 </label>
                 <br /> 
@@ -204,68 +203,68 @@ const Form = () => {
                 </label>
                 <br /> 
                 <label htmlFor="italian">
-                    <input type="checkbox" name="italian" id="italian" onChange={inputChange} />
+                    <input type="checkbox" name="italian" id="italian" onChange={inputChange} checked={orderState.italian} />
                     Spicy Italian Sausage
                 </label>
                 <br /> 
                 <label htmlFor="chicken">
-                    <input type="checkbox" name="chicken" id="chicken" onChange={inputChange} />
+                    <input type="checkbox" name="chicken" id="chicken" onChange={inputChange} checked={orderState.chicken} />
                     Grilled Chicken
                 </label>
                 <br /> 
                 <label htmlFor="onions">
-                    <input type="checkbox" name="onions" id="onions" onChange={inputChange} />
+                    <input type="checkbox" name="onions" id="onions" onChange={inputChange} checked={orderState.onions} />
                     Onions
                 </label>
                 <br /> 
                 <label htmlFor="pepper">
-                    <input type="checkbox" name="pepper" id="pepper" onChange={inputChange} />
+                    <input type="checkbox" name="pepper" id="pepper" onChange={inputChange} checked={orderState.pepper} />
                     Green Pepper
                 </label>
                 <br /> 
                 <label htmlFor="tomatoes">
-                    <input type="checkbox" name="tomatoes" id="tomatoes" onChange={inputChange} />
+                    <input type="checkbox" name="tomatoes" id="tomatoes" onChange={inputChange} checked={orderState.tomatoes} />
                     Diced Tomatoes
                 </label>
                 <br /> 
                 <label htmlFor="olives">
-                    <input type="checkbox" name="olives" id="olives" onChange={inputChange} />
+                    <input type="checkbox" name="olives" id="olives" onChange={inputChange} checked={orderState.olives} />
                     Black Olives
                 </label>
                 <br /> 
                 <label htmlFor="roastedGarlic">
-                    <input type="checkbox" name="roastedGarlic" id="roastedGarlic" onChange={inputChange} />
+                    <input type="checkbox" name="roastedGarlic" id="roastedGarlic" onChange={inputChange} checked={orderState.roastedGarlic}/>
                     Roasted Garlic
                 </label>
                 <br /> 
                 <label htmlFor="artichoke">
-                    <input type="checkbox" name="artichoke" id="artichoke" onChange={inputChange} />
+                    <input type="checkbox" name="artichoke" id="artichoke" onChange={inputChange} checked={orderState.artichoke} />
                     Artichoke Hearts
                 </label>
                 <br /> 
                 <label htmlFor="cheese">
-                    <input type="checkbox" name="cheese" id="cheese" onChange={inputChange} />
+                    <input type="checkbox" name="cheese" id="cheese" onChange={inputChange} checked={orderState.cheese}/>
                     Three Cheese
                 </label>
                 <br /> 
                 <label htmlFor="pineapple">
-                    <input type="checkbox" name="pineapple" id="pineapple" onChange={inputChange} />
+                    <input type="checkbox" name="pineapple" id="pineapple" onChange={inputChange} checked={orderState.pineapple} />
                     Pineapple
                 </label>
                 <br />  
                 <label htmlFor="extraCheese">
-                    <input type="checkbox" name="extraCheese" id="extraCheese" onChange={inputChange} />
+                    <input type="checkbox" name="extraCheese" id="extraCheese" onChange={inputChange} checked={orderState.extraCheese} />
                     Extra Cheese
                 </label>
                 <br /> 
-                {/* gluyten free would go here */}
+                {/* gluten option would go here */}
                 <label htmlFor='instructions'>
                     Special Instructions: <br /> 
-                    <textarea placeholder="Anything else you'd like to add?" name="instructions" id="instructions" onChange={inputChange} />
+                    <textarea placeholder="Anything else you'd like to add?" name="instructions" id="instructions" onChange={inputChange} value={orderState.instructions}/>
                 </label>
                 <br /> 
                 <label htmlFor='quantity'>
-                    <select id="quantity" name="quantity" onChange={inputChange}>
+                    <select id="quantity" name="quantity" onChange={inputChange} value={orderState.quantity}>
                         <option>Select Quantity</option>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
@@ -274,7 +273,9 @@ const Form = () => {
                     </select>
                 </label>
                 <br /> 
-                <button disabled={disabledButton}>Add to Order</button>
+                <button >Add to Order</button>
+
+                <pre>{JSON.stringify(ordered, null, 2)}</pre>
 
 
 
